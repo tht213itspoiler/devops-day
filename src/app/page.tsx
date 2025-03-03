730b4be1501cb642610296eb629eb85cc4f64e30
Iris <trongtruong@gmail.com>
2025-03-03 10:57:34 +0700

    update endava logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,15 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
+          src="/endava.svg"
+          alt="Endava logo"
           width={180}
           height={38}
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Hello - This is new pipeline -Not really{" "}
+            We build it - We run it{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               src/app/page.tsx
             </code>
