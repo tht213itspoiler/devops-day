585b05a3a27fa82643e5956bf13f8f0580558cf5
Iris <trongtruong@gmail.com>
2025-03-05 16:22:25 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,12 +32,12 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/vercel.svg"
+              src="/endava_icon.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
             />
-            Vist us
+            Vist us now
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
